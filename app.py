41561dde2f857f51074db79d908796652653a387
psiriyapuraju <psiriyapuraju@gmail.com>
2025-06-28 15:48:18 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import numpy as np
 from flask import Flask, request, render_template
 import pickle
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(current_dir, 'model.pkl')
+
+with open(model_path, 'rb') as f:
+    model = pickle.load(f)
 
 app = Flask(__name__)
 
-path = "C:\\Users\\psiri\\Desktop\\OptiCrop\\"
-model = pickle.load(open(path + "model.pkl", 'rb'))
+# path = "C:\\Users\\psiri\\Desktop\\OptiCrop\\"
+# model = pickle.load(open(path + "model.pkl", 'rb'))
 
 @app.route('/')
 
