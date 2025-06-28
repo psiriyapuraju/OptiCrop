e1ce7a832682597c6f452ffdcd9e43b842400f01
psiriyapuraju <psiriyapuraju@gmail.com>
2025-06-28 21:04:10 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,21 +1,13 @@
 import numpy as np
 from flask import Flask, request, render_template
 import pickle
-#import os
-
-#current_dir = os.path.dirname(os.path.abspath(__file__))
-#model_path = os.path.join(current_dir, 'model.pkl')
-
-#with open(model_path, 'rb') as f:
-#    model = pickle.load(f)
 
 app = Flask(__name__)
 
-path = r"C:\\Users\\psiri\\Desktop\\OptiCrop\\"
-model = pickle.load(open(path + "model.pkl", 'rb'))
+path = r"C:\Users\psiri\Desktop\OptiCrop\\"
+model = pickle.load(open(path + 'model.pkl', 'rb'))
 
 @app.route('/')
-
 def home():
     return render_template('home.html')
 
@@ -27,13 +19,14 @@ def about():
 def findyourcrop():
     return render_template('findyourcrop.html')
 
-@app.route('/predict', methods = ['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     int_features = [float(x) for x in request.form.values()]
     features = [np.array(int_features)]
     prediction = model.predict(features)
-    output = prediction [0]
-    return render_template('findyourcrop.html',prediction_text='Best crop for given conditions is {}'.format(output))
+    
+    output = prediction[0]
+    return render_template('findyourcrop.html', prediction_text=f"Best crop for given conditions is {output}")
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True, use_reloader=False)
